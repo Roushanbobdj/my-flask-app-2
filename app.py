9bc7ee070c44ede45e0392ef3c68845f5eb93a40
Roushanbobdj <panditrrr77@gmail.com>
2026-03-03 18:15:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1145,10 +1145,4 @@ with app.app_context():
 # -------------------------
 # RUN APP
 # -------------------------
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)  
-
-
-
 
