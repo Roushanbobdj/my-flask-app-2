e7215d9df34c4efb4911b86c4982364e4c29674d
Roushanbobdj <panditrrr77@gmail.com>
2026-03-05 10:28:33 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,28 @@
-self.addEventListener("install", function () {
-  console.log("PWA Installed");
+const CACHE_NAME = "rr-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/login",
+  "/static/favicon.png",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log("Cache opened");
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener("fetch", function () {});
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
